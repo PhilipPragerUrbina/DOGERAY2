84bbf96718c7e628b04676c89c10353d472e2eaa
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2022-03-26 13:41:43 -0700

    A few improvements

--- classes/vec3.hpp
+++ classes/vec3.hpp
@@ -32,22 +32,22 @@ public:
 
     //operations on vector
     //returns dot product of current vector and other vector
-    __host__ __device__ float dot(Vec3 b)
+    __host__ __device__ float dot(const Vec3& b)
     const {
-        return values[0] * b[0] + values[1] * b[1] + values[2] * b[2];
+        return (values[0] * b[0]) + (values[1] * b[1]) + (values[2] * b[2]);
     }
     //get cross product(right hand rule :)! )
-    __host__ __device__ Vec3 cross(Vec3 b)
+    __host__ __device__ Vec3 cross(const Vec3& b)
     const {
         return Vec3(values[1] * b[2] - values[2] * b[1], values[2] * b[0] - values[0] * b[2], values[0] * b[1] - values[1] * b[0]);
     }
     //returns normalized version of vector
-    __host__ __device__ Vec3 normalized() {
-        return *this * Vec3(1.0f / length());
+    __host__ __device__ Vec3 normalized() const {
+        return Vec3(*this) / length();
     }
     //length
-    __host__ __device__ float length()  {
-        return sqrtf(this->dot(*this));
+    __host__ __device__ float length() const {
+        return sqrtf(dot(*this));
     }
     //clamp vector between two values
     __host__ __device__ Vec3 clamped(float min, float max) const {
@@ -59,36 +59,40 @@ public:
         return out;
     }
     //reflect vector ober other vector(normal)
-    __host__ __device__ Vec3 reflected(Vec3 n) {
-        return *this  - Vec3(2.0f * this->dot(n)) * n  ;
+    __host__ __device__ Vec3 reflected(const Vec3& n) const  {
+        return Vec3(*this) - (n * 2.0 * dot(n));
+    }
+    //change order
+    __host__ __device__ Vec3 inverse() const {
+        return Vec3(values[2], values[1], values[0]);
     }
 
     //gets minimum and max values of two vectors
-    Vec3 min(Vec3 b) const{
+    Vec3 min(const Vec3& b) const{
         return Vec3(fmin(values[0], b[0]), fmin(values[1], b[1]), fmin(values[2], b[2]));
     }
-    Vec3 max(Vec3 b) const{
+    Vec3 max(const Vec3& b) const{
         return Vec3(fmax(values[0], b[0]), fmax(values[1], b[1]), fmax(values[2], b[2]));
     }
 
     //operators
     //access x like Vec3[0] for conveince. This is better thnat vec2.x since you can use loops
-    __host__ __device__ constexpr float& operator[](int x) {
-        return values[x];
+    __host__ __device__  inline  float operator [](int i) const { return values[i]; }
+    __host__ __device__  inline  float& operator [](int i) { return values[i]; }
+    __host__ __device__ inline  Vec3 operator+(const Vec3& b) const {
+        return Vec3(values[0] + b.values[0],values[1] + b.values[1],values[2] + b.values[2]);
     }
-    __host__ __device__ Vec3 operator+(const Vec3& b) const {
-        return Vec3(this->values[0] + b.values[0], this->values[1] + b.values[1], this->values[2] + b.values[2]);
+    __host__ __device__ inline  Vec3 operator-(const Vec3& b) const {
+        return Vec3(values[0] - b.values[0],values[1] - b.values[1], values[2] - b.values[2]);
     }
-    __host__ __device__ Vec3 operator-(const Vec3& b) const {
-        return Vec3(this->values[0] - b.values[0], this->values[1] - b.values[1], this->values[2] - b.values[2]);
+    __host__ __device__ inline  Vec3 operator*(const Vec3& b) const{
+        return Vec3(values[0] * b.values[0], values[1] * b.values[1], values[2] * b.values[2]);
     }
-    __host__ __device__ Vec3 operator*(const Vec3& b) const{
-        return Vec3(this->values[0] * b.values[0], this->values[1] * b.values[1], this->values[2] * b.values[2]);
+    __host__ __device__ inline  Vec3 operator/(const Vec3& b) const {
+        return Vec3(values[0] / b.values[0],values[1] / b.values[1],values[2] / b.values[2]);
     }
-    __host__ __device__ Vec3 operator/(const Vec3& b) const {
-        return Vec3(this->values[0] / b.values[0], this->values[1] / b.values[1], this->values[2] / b.values[2]);
-    }
-    __host__ __device__ int extent() {
+    __host__ __device__ Vec3 operator-() const { return Vec3(-values[0], -values[1], -values[2]); }
+    __host__ __device__ int extent() const{
         int max = 0;
         if (values[1] > values[0]) { max = 1; }
         if (values[2] > values[0] && values[2] > values[1]) { max = 2; }
